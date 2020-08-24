7776949b04f82b67336a2987617b8fafce8a290f
luqmanulhakim96 <hakim9608@gmail.com>
2020-08-24 12:52:28 +0800

    Update EmailUserFormAdminDNull.php

--- app/Mail/Form/EmailUserFormAdminDNull.php
+++ app/Mail/Form/EmailUserFormAdminDNull.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class EmailUserFormAdminCNull extends Mailable
+class EmailUserFormAdminDNull extends Mailable
 {
     use Queueable, SerializesModels;
 
